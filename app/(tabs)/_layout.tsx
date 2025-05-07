7a2d764caf1c7bd600f31586c4d5999b60dca79f
DougDelaney <xplorr@pm.me>
2025-05-06 23:31:30 -0700

    remove: remove Explore tab and associated navigation options from tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -42,13 +42,6 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
         name="two"
         options={{
           title: 'Saved Clips',
